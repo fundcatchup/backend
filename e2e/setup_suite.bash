91421ee372fe40bcbdcd33bbefc998e17bcc067b
Sandipan Dey <hey@sandipan.dev>
2024-01-08 11:48:49 +0530

    ci: add e2e test framework

---
+++ e2e/setup_suite.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export REPO_ROOT=$(git rev-parse --show-toplevel)
+source "${REPO_ROOT}/e2e/helpers/_common.bash"
+
+TILT_PID_FILE=$REPO_ROOT/e2e/.tilt_pid
+
+setup_suite() {
+  background buck2 run //dev:up > "${REPO_ROOT}/e2e/.e2e-tilt.log"
+  echo $! > "$TILT_PID_FILE"
+  await_api_is_up
+}
+
+teardown_suite() {
+  if [[ -f "$TILT_PID_FILE" ]]; then
+    kill "$(cat "$TILT_PID_FILE")" > /dev/null || true
+  fi
+
+  buck2 run //dev:down
+}
+
+await_api_is_up() {
+  server_is_up() {
+    exec_graphql 'anon' 'globals'
+    version="$(graphql_output '.data.globals.version')"
+    [[ "${version}" = "0.0.0-development" ]] || exit 1
+  }
+
+  retry 300 1 server_is_up
+}
